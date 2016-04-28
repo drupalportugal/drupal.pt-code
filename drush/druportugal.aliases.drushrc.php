c2942ed955b285d07ed8723afcea876218d20b79
Luís Pedro Algarvio <lp.algarvio@gmail.com>
2016-04-28 11:17:03 +0100

    Added drush scripts and aliases

---
+++ drush/druportugal.aliases.drushrc.php
@@ -0,0 +1,67 @@
+<?php
+
+if (!isset($drush_major_version)) {
+  $drush_version_components = explode('.', DRUSH_VERSION);
+  $drush_major_version = $drush_version_components[0];
+}
+// Site druportugal, environment dev
+$aliases['dev'] = array(
+  'root' => '/var/www/html/druportugal.dev/docroot',
+  'ac-site' => 'druportugal',
+  'ac-env' => 'dev',
+  'ac-realm' => 'prod',
+  'uri' => 'druportugaldev.prod.acquia-sites.com',
+  'remote-host' => 'staging-5196.prod.hosting.acquia.com',
+  'remote-user' => 'druportugal.dev',
+  'path-aliases' => array(
+    '%drush-script' => 'drush' . $drush_major_version,
+  )
+);
+$aliases['dev.livedev'] = array(
+  'parent' => '@druportugal.dev',
+  'root' => '/mnt/gfs/druportugal.dev/livedev/docroot',
+);
+
+if (!isset($drush_major_version)) {
+  $drush_version_components = explode('.', DRUSH_VERSION);
+  $drush_major_version = $drush_version_components[0];
+}
+// Site druportugal, environment prod
+$aliases['prod'] = array(
+  'root' => '/var/www/html/druportugal.prod/docroot',
+  'ac-site' => 'druportugal',
+  'ac-env' => 'prod',
+  'ac-realm' => 'prod',
+  'uri' => 'druportugal.prod.acquia-sites.com',
+  'remote-host' => 'ded-5779.prod.hosting.acquia.com',
+  'remote-user' => 'druportugal.prod',
+  'path-aliases' => array(
+    '%drush-script' => 'drush' . $drush_major_version,
+  )
+);
+$aliases['prod.livedev'] = array(
+  'parent' => '@druportugal.prod',
+  'root' => '/mnt/gfs/druportugal.prod/livedev/docroot',
+);
+
+if (!isset($drush_major_version)) {
+  $drush_version_components = explode('.', DRUSH_VERSION);
+  $drush_major_version = $drush_version_components[0];
+}
+// Site druportugal, environment test
+$aliases['test'] = array(
+  'root' => '/var/www/html/druportugal.test/docroot',
+  'ac-site' => 'druportugal',
+  'ac-env' => 'test',
+  'ac-realm' => 'prod',
+  'uri' => 'druportugalstg.prod.acquia-sites.com',
+  'remote-host' => 'staging-5197.prod.hosting.acquia.com',
+  'remote-user' => 'druportugal.test',
+  'path-aliases' => array(
+    '%drush-script' => 'drush' . $drush_major_version,
+  )
+);
+$aliases['test.livedev'] = array(
+  'parent' => '@druportugal.test',
+  'root' => '/mnt/gfs/druportugal.test/livedev/docroot',
+);
